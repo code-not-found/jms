21b4fbdc685cf666f4d1e35fb689795ef79379d7
codenotfound <codenotfound.com@gmail.com>
2018-01-04 21:49:25 +0100

    format code

--- jms-activemq-priority/src/main/java/com/codenotfound/jms/priority/Consumer.java
+++ jms-activemq-priority/src/main/java/com/codenotfound/jms/priority/Consumer.java
@@ -15,49 +15,51 @@ import org.slf4j.LoggerFactory;
 
 public class Consumer {
 
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(Consumer.class);
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(Consumer.class);
 
-    private Connection connection;
-    private Session session;
-    private MessageConsumer consumer;
+  private Connection connection;
+  private Session session;
+  private MessageConsumer consumer;
 
-    public void openConnection() throws JMSException {
-        // Create a new connection factory
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-                "tcp://0.0.0.0:61616?jms.messagePrioritySupported=true");
-        connection = connectionFactory.createConnection();
-    }
+  public void openConnection() throws JMSException {
+    // Create a new connection factory
+    ConnectionFactory connectionFactory =
+        new ActiveMQConnectionFactory(
+            "tcp://0.0.0.0:61616?jms.messagePrioritySupported=true");
+    connection = connectionFactory.createConnection();
+  }
 
-    public void closeConnection() throws JMSException {
-        connection.close();
-    }
+  public void closeConnection() throws JMSException {
+    connection.close();
+  }
 
-    public void createConsumer(String queue) throws JMSException {
-        // Create a session for receiving messages
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+  public void createConsumer(String queue) throws JMSException {
+    // Create a session for receiving messages
+    session =
+        connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        // Create the destination from which messages will be received
-        Destination destination = session.createQueue(queue);
+    // Create the destination from which messages will be received
+    Destination destination = session.createQueue(queue);
 
-        // Create a MessageConsumer for receiving messages
-        consumer = session.createConsumer(destination);
+    // Create a MessageConsumer for receiving messages
+    consumer = session.createConsumer(destination);
 
-        // Start the connection in order to receive messages
-        connection.start();
-    }
+    // Start the connection in order to receive messages
+    connection.start();
+  }
 
-    public String receive(int timeout) throws JMSException {
-        // Read a message from the destination
-        Message message = consumer.receive(timeout);
+  public String receive(int timeout) throws JMSException {
+    // Read a message from the destination
+    Message message = consumer.receive(timeout);
 
-        // Cast the message to the correct type
-        TextMessage input = (TextMessage) message;
+    // Cast the message to the correct type
+    TextMessage input = (TextMessage) message;
 
-        // Retrieve the message content
-        String text = input.getText();
-        LOGGER.info("{} received", text);
+    // Retrieve the message content
+    String text = input.getText();
+    LOGGER.info("{} received", text);
 
-        return text;
-    }
+    return text;
+  }
 }
--- jms-activemq-priority/src/main/java/com/codenotfound/jms/priority/Producer.java
+++ jms-activemq-priority/src/main/java/com/codenotfound/jms/priority/Producer.java
@@ -16,48 +16,50 @@ import org.slf4j.LoggerFactory;
 
 public class Producer {
 
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(Producer.class);
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(Producer.class);
 
-    private Connection connection;
-    private Session session;
-    private MessageProducer producer;
+  private Connection connection;
+  private Session session;
+  private MessageProducer producer;
 
-    public void openConnection() throws JMSException {
-        // Create a new connection factory
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-                ActiveMQConnection.DEFAULT_BROKER_URL);
-        connection = connectionFactory.createConnection();
-    }
+  public void openConnection() throws JMSException {
+    // Create a new connection factory
+    ConnectionFactory connectionFactory =
+        new ActiveMQConnectionFactory(
+            ActiveMQConnection.DEFAULT_BROKER_URL);
+    connection = connectionFactory.createConnection();
+  }
 
-    public void closeConnection() throws JMSException {
-        connection.close();
-    }
+  public void closeConnection() throws JMSException {
+    connection.close();
+  }
 
-    public void createProducer(String queue) throws JMSException {
-        // Create a session for sending messages
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+  public void createProducer(String queue) throws JMSException {
+    // Create a session for sending messages
+    session =
+        connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        // Create the destination to which messages will be sent
-        Destination destination = session.createQueue(queue);
+    // Create the destination to which messages will be sent
+    Destination destination = session.createQueue(queue);
 
-        // Create a MessageProducer for sending the messages
-        producer = session.createProducer(destination);
-    }
+    // Create a MessageProducer for sending the messages
+    producer = session.createProducer(destination);
+  }
 
-    public void send(String text) throws JMSException {
-        TextMessage message = session.createTextMessage(text);
-        producer.send(message);
+  public void send(String text) throws JMSException {
+    TextMessage message = session.createTextMessage(text);
+    producer.send(message);
 
-        LOGGER.info("{} sent with default priority(=4)", text);
-    }
+    LOGGER.info("{} sent with default priority(=4)", text);
+  }
 
-    public void send(String text, int priority) throws JMSException {
-        TextMessage message = session.createTextMessage(text);
-        // Note: setting the priority directly on the JMS Message does not work
-        // as in that case the priority of the producer is taken
-        producer.send(message, DeliveryMode.PERSISTENT, priority, 0);
+  public void send(String text, int priority) throws JMSException {
+    TextMessage message = session.createTextMessage(text);
+    // Note: setting the priority directly on the JMS Message does not work
+    // as in that case the priority of the producer is taken
+    producer.send(message, DeliveryMode.PERSISTENT, priority, 0);
 
-        LOGGER.info("{} sent with priority={}", text, priority);
-    }
+    LOGGER.info("{} sent with priority={}", text, priority);
+  }
 }
