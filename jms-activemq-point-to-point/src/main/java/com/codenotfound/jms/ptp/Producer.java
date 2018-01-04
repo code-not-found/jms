52488517b41e847dd71f708faed045add25829ac
codenotfound <codenotfound.com@gmail.com>
2018-01-04 21:18:41 +0100

    format code

--- jms-activemq-point-to-point/src/main/java/com/codenotfound/jms/ptp/Producer.java
+++ jms-activemq-point-to-point/src/main/java/com/codenotfound/jms/ptp/Producer.java
@@ -15,48 +15,52 @@ import org.slf4j.LoggerFactory;
 
 public class Producer {
 
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(Producer.class);
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(Producer.class);
 
-    private String clientId;
-    private Connection connection;
-    private Session session;
-    private MessageProducer messageProducer;
+  private String clientId;
+  private Connection connection;
+  private Session session;
+  private MessageProducer messageProducer;
 
-    public void create(String clientId, String queueName) throws JMSException {
-        this.clientId = clientId;
+  public void create(String clientId, String queueName)
+      throws JMSException {
+    this.clientId = clientId;
 
-        // create a Connection Factory
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-                ActiveMQConnection.DEFAULT_BROKER_URL);
+    // create a Connection Factory
+    ConnectionFactory connectionFactory =
+        new ActiveMQConnectionFactory(
+            ActiveMQConnection.DEFAULT_BROKER_URL);
 
-        // create a Connection
-        connection = connectionFactory.createConnection();
-        connection.setClientID(clientId);
+    // create a Connection
+    connection = connectionFactory.createConnection();
+    connection.setClientID(clientId);
 
-        // create a Session
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    // create a Session
+    session =
+        connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        // create the Queue to which messages will be sent
-        Queue queue = session.createQueue(queueName);
+    // create the Queue to which messages will be sent
+    Queue queue = session.createQueue(queueName);
 
-        // create a MessageProducer for sending messages
-        messageProducer = session.createProducer(queue);
-    }
+    // create a MessageProducer for sending messages
+    messageProducer = session.createProducer(queue);
+  }
 
-    public void closeConnection() throws JMSException {
-        connection.close();
-    }
+  public void closeConnection() throws JMSException {
+    connection.close();
+  }
 
-    public void sendName(String firstName, String lastName) throws JMSException {
-        String text = firstName + " " + lastName;
+  public void sendName(String firstName, String lastName)
+      throws JMSException {
+    String text = firstName + " " + lastName;
 
-        // create a JMS TextMessage
-        TextMessage textMessage = session.createTextMessage(text);
+    // create a JMS TextMessage
+    TextMessage textMessage = session.createTextMessage(text);
 
-        // send the message to the queue destination
-        messageProducer.send(textMessage);
+    // send the message to the queue destination
+    messageProducer.send(textMessage);
 
-        LOGGER.debug(clientId + ": sent message with text='{}'", text);
-    }
+    LOGGER.debug(clientId + ": sent message with text='{}'", text);
+  }
 }
--- jms-activemq-point-to-point/src/test/java/com/codenotfound/jms/ptp/ConsumerTest.java
+++ jms-activemq-point-to-point/src/test/java/com/codenotfound/jms/ptp/ConsumerTest.java
@@ -12,137 +12,145 @@ import org.junit.Test;
 
 public class ConsumerTest {
 
-    private static Producer producerPointToPoint, producerOnlyOneConsumer,
-            producerNoTimingDependencies, producerAcknowledgeProcessing;
-    private static Consumer consumerPointToPoint, consumer1OnlyOneConsumer,
-            consumer2OnlyOneConsumer, consumerNoTimingDependencies,
-            consumer1AcknowledgeProcessing, consumer2AcknowledgeProcessing;
+  private static Producer producerPointToPoint,
+      producerOnlyOneConsumer, producerNoTimingDependencies,
+      producerAcknowledgeProcessing;
+  private static Consumer consumerPointToPoint,
+      consumer1OnlyOneConsumer, consumer2OnlyOneConsumer,
+      consumerNoTimingDependencies, consumer1AcknowledgeProcessing,
+      consumer2AcknowledgeProcessing;
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws JMSException, NamingException {
-        producerPointToPoint = new Producer();
-        producerPointToPoint.create("producer-pointtopoint", "pointtopoint.q");
+  @BeforeClass
+  public static void setUpBeforeClass()
+      throws JMSException, NamingException {
+    producerPointToPoint = new Producer();
+    producerPointToPoint.create("producer-pointtopoint",
+        "pointtopoint.q");
 
-        producerOnlyOneConsumer = new Producer();
-        producerOnlyOneConsumer.create("producer-onlyoneconsumer",
-                "onlyoneconsumer.q");
+    producerOnlyOneConsumer = new Producer();
+    producerOnlyOneConsumer.create("producer-onlyoneconsumer",
+        "onlyoneconsumer.q");
 
-        producerNoTimingDependencies = new Producer();
-        producerNoTimingDependencies.create("producer-notimingdependencies",
-                "notimingdependencies.q");
+    producerNoTimingDependencies = new Producer();
+    producerNoTimingDependencies.create(
+        "producer-notimingdependencies", "notimingdependencies.q");
 
-        producerAcknowledgeProcessing = new Producer();
-        producerAcknowledgeProcessing.create("producer-acknowledgeprocessing",
-                "acknowledgeprocessing.q");
+    producerAcknowledgeProcessing = new Producer();
+    producerAcknowledgeProcessing.create(
+        "producer-acknowledgeprocessing", "acknowledgeprocessing.q");
 
-        consumerPointToPoint = new Consumer();
-        consumerPointToPoint.create("consumer-pointtopoint", "pointtopoint.q");
+    consumerPointToPoint = new Consumer();
+    consumerPointToPoint.create("consumer-pointtopoint",
+        "pointtopoint.q");
 
-        consumer1OnlyOneConsumer = new Consumer();
-        consumer1OnlyOneConsumer.create("consumer1-onlyoneconsumer",
-                "onlyoneconsumer.q");
+    consumer1OnlyOneConsumer = new Consumer();
+    consumer1OnlyOneConsumer.create("consumer1-onlyoneconsumer",
+        "onlyoneconsumer.q");
 
-        consumer2OnlyOneConsumer = new Consumer();
-        consumer2OnlyOneConsumer.create("consumer2-onlyoneconsumer",
-                "onlyoneconsumer.q");
+    consumer2OnlyOneConsumer = new Consumer();
+    consumer2OnlyOneConsumer.create("consumer2-onlyoneconsumer",
+        "onlyoneconsumer.q");
 
-        // consumerNoTimingDependencies
+    // consumerNoTimingDependencies
 
-        consumer1AcknowledgeProcessing = new Consumer();
-        consumer1AcknowledgeProcessing.create(
-                "consumer1-acknowledgeprocessing", "acknowledgeprocessing.q");
+    consumer1AcknowledgeProcessing = new Consumer();
+    consumer1AcknowledgeProcessing.create(
+        "consumer1-acknowledgeprocessing", "acknowledgeprocessing.q");
 
-        consumer2AcknowledgeProcessing = new Consumer();
-        consumer2AcknowledgeProcessing.create(
-                "consumer2-acknowledgeprocessing", "acknowledgeprocessing.q");
+    consumer2AcknowledgeProcessing = new Consumer();
+    consumer2AcknowledgeProcessing.create(
+        "consumer2-acknowledgeprocessing", "acknowledgeprocessing.q");
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws JMSException {
+    producerPointToPoint.closeConnection();
+    producerOnlyOneConsumer.closeConnection();
+    producerNoTimingDependencies.closeConnection();
+    producerAcknowledgeProcessing.closeConnection();
+
+    consumerPointToPoint.closeConnection();
+    consumer1OnlyOneConsumer.closeConnection();
+    consumer2OnlyOneConsumer.closeConnection();
+    consumerNoTimingDependencies.closeConnection();
+    // consumer1AcknowledgeProcessing
+    consumer2AcknowledgeProcessing.closeConnection();
+  }
+
+  @Test
+  public void testGetGreeting() {
+    try {
+      producerPointToPoint.sendName("Frodo", "Baggins");
+
+      String greeting = consumerPointToPoint.getGreeting(1000, true);
+      assertEquals("Hello Frodo Baggins!", greeting);
+
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
     }
+  }
 
-    @AfterClass
-    public static void tearDownAfterClass() throws JMSException {
-        producerPointToPoint.closeConnection();
-        producerOnlyOneConsumer.closeConnection();
-        producerNoTimingDependencies.closeConnection();
-        producerAcknowledgeProcessing.closeConnection();
+  @Test
+  public void testOnlyOneConsumer() throws InterruptedException {
+    try {
+      producerOnlyOneConsumer.sendName("Legolas", "Greenleaf");
 
-        consumerPointToPoint.closeConnection();
-        consumer1OnlyOneConsumer.closeConnection();
-        consumer2OnlyOneConsumer.closeConnection();
-        consumerNoTimingDependencies.closeConnection();
-        // consumer1AcknowledgeProcessing
-        consumer2AcknowledgeProcessing.closeConnection();
+      String greeting1 =
+          consumer1OnlyOneConsumer.getGreeting(1000, true);
+      assertEquals("Hello Legolas Greenleaf!", greeting1);
+
+      Thread.sleep(1000);
+
+      String greeting2 =
+          consumer2OnlyOneConsumer.getGreeting(1000, true);
+      // each message has only one consumer
+      assertEquals("no greeting", greeting2);
+
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
     }
+  }
 
-    @Test
-    public void testGetGreeting() {
-        try {
-            producerPointToPoint.sendName("Frodo", "Baggins");
+  @Test
+  public void testNoTimingDependencies() {
+    try {
+      producerNoTimingDependencies.sendName("Samwise", "Gamgee");
+      // a receiver can fetch the message whether or not it was running
+      // when the client sent the message
+      consumerNoTimingDependencies = new Consumer();
+      consumerNoTimingDependencies.create(
+          "consumer-notimingdependencies", "notimingdependencies.q");
 
-            String greeting = consumerPointToPoint.getGreeting(1000, true);
-            assertEquals("Hello Frodo Baggins!", greeting);
+      String greeting =
+          consumerNoTimingDependencies.getGreeting(1000, true);
+      assertEquals("Hello Samwise Gamgee!", greeting);
 
-        } catch (JMSException e) {
-            fail("a JMS Exception occurred");
-        }
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
     }
+  }
 
-    @Test
-    public void testOnlyOneConsumer() throws InterruptedException {
-        try {
-            producerOnlyOneConsumer.sendName("Legolas", "Greenleaf");
+  @Test
+  public void testAcknowledgeProcessing()
+      throws InterruptedException {
+    try {
+      producerAcknowledgeProcessing.sendName("Gandalf", "the Grey");
 
-            String greeting1 = consumer1OnlyOneConsumer.getGreeting(1000, true);
-            assertEquals("Hello Legolas Greenleaf!", greeting1);
+      // consume the message without an acknowledgment
+      String greeting1 =
+          consumer1AcknowledgeProcessing.getGreeting(1000, false);
+      assertEquals("Hello Gandalf the Grey!", greeting1);
 
-            Thread.sleep(1000);
+      // close the MessageConsumer so the broker knows there is no
+      // acknowledgment
+      consumer1AcknowledgeProcessing.closeConnection();
 
-            String greeting2 = consumer2OnlyOneConsumer.getGreeting(1000, true);
-            // each message has only one consumer
-            assertEquals("no greeting", greeting2);
+      String greeting2 =
+          consumer2AcknowledgeProcessing.getGreeting(1000, true);
+      assertEquals("Hello Gandalf the Grey!", greeting2);
 
-        } catch (JMSException e) {
-            fail("a JMS Exception occurred");
-        }
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
     }
-
-    @Test
-    public void testNoTimingDependencies() {
-        try {
-            producerNoTimingDependencies.sendName("Samwise", "Gamgee");
-            // a receiver can fetch the message whether or not it was running
-            // when the client sent the message
-            consumerNoTimingDependencies = new Consumer();
-            consumerNoTimingDependencies.create(
-                    "consumer-notimingdependencies", "notimingdependencies.q");
-
-            String greeting = consumerNoTimingDependencies.getGreeting(1000,
-                    true);
-            assertEquals("Hello Samwise Gamgee!", greeting);
-
-        } catch (JMSException e) {
-            fail("a JMS Exception occurred");
-        }
-    }
-
-    @Test
-    public void testAcknowledgeProcessing() throws InterruptedException {
-        try {
-            producerAcknowledgeProcessing.sendName("Gandalf", "the Grey");
-
-            // consume the message without an acknowledgment
-            String greeting1 = consumer1AcknowledgeProcessing.getGreeting(1000,
-                    false);
-            assertEquals("Hello Gandalf the Grey!", greeting1);
-
-            // close the MessageConsumer so the broker knows there is no
-            // acknowledgment
-            consumer1AcknowledgeProcessing.closeConnection();
-
-            String greeting2 = consumer2AcknowledgeProcessing.getGreeting(1000,
-                    true);
-            assertEquals("Hello Gandalf the Grey!", greeting2);
-
-        } catch (JMSException e) {
-            fail("a JMS Exception occurred");
-        }
-    }
+  }
 }
