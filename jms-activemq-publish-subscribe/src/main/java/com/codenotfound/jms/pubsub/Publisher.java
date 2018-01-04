b8ffa247c8c8d71d87864b5d53065066fef0b9ad
codenotfound <codenotfound.com@gmail.com>
2018-01-04 22:11:59 +0100

    format code

--- jms-activemq-publish-subscribe/src/main/java/com/codenotfound/jms/pubsub/Publisher.java
+++ jms-activemq-publish-subscribe/src/main/java/com/codenotfound/jms/pubsub/Publisher.java
@@ -15,48 +15,52 @@ import org.slf4j.LoggerFactory;
 
 public class Publisher {
 
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(Publisher.class);
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(Publisher.class);
 
-    private String clientId;
-    private Connection connection;
-    private Session session;
-    private MessageProducer messageProducer;
+  private String clientId;
+  private Connection connection;
+  private Session session;
+  private MessageProducer messageProducer;
 
-    public void create(String clientId, String topicName) throws JMSException {
-        this.clientId = clientId;
+  public void create(String clientId, String topicName)
+      throws JMSException {
+    this.clientId = clientId;
 
-        // create a Connection Factory
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-                ActiveMQConnection.DEFAULT_BROKER_URL);
+    // create a Connection Factory
+    ConnectionFactory connectionFactory =
+        new ActiveMQConnectionFactory(
+            ActiveMQConnection.DEFAULT_BROKER_URL);
 
-        // create a Connection
-        connection = connectionFactory.createConnection();
-        connection.setClientID(clientId);
+    // create a Connection
+    connection = connectionFactory.createConnection();
+    connection.setClientID(clientId);
 
-        // create a Session
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    // create a Session
+    session =
+        connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        // create the Topic to which messages will be sent
-        Topic topic = session.createTopic(topicName);
+    // create the Topic to which messages will be sent
+    Topic topic = session.createTopic(topicName);
 
-        // create a MessageProducer for sending messages
-        messageProducer = session.createProducer(topic);
-    }
+    // create a MessageProducer for sending messages
+    messageProducer = session.createProducer(topic);
+  }
 
-    public void closeConnection() throws JMSException {
-        connection.close();
-    }
+  public void closeConnection() throws JMSException {
+    connection.close();
+  }
 
-    public void sendName(String firstName, String lastName) throws JMSException {
-        String text = firstName + " " + lastName;
+  public void sendName(String firstName, String lastName)
+      throws JMSException {
+    String text = firstName + " " + lastName;
 
-        // create a JMS TextMessage
-        TextMessage textMessage = session.createTextMessage(text);
+    // create a JMS TextMessage
+    TextMessage textMessage = session.createTextMessage(text);
 
-        // send the message to the topic destination
-        messageProducer.send(textMessage);
+    // send the message to the topic destination
+    messageProducer.send(textMessage);
 
-        LOGGER.debug(clientId + ": sent message with text='{}'", text);
-    }
+    LOGGER.debug(clientId + ": sent message with text='{}'", text);
+  }
 }
--- jms-activemq-publish-subscribe/src/test/java/com/codenotfound/jms/pubsub/DurableSubscriberTest.java
+++ jms-activemq-publish-subscribe/src/test/java/com/codenotfound/jms/pubsub/DurableSubscriberTest.java
@@ -11,94 +11,100 @@ import org.junit.Test;
 
 public class DurableSubscriberTest {
 
-    private static Publisher publisherPublishSubscribe,
-            publisherDurableSubscriber;
-    private static DurableSubscriber subscriberPublishSubscribe,
+  private static Publisher publisherPublishSubscribe,
+      publisherDurableSubscriber;
+  private static DurableSubscriber subscriberPublishSubscribe,
 
-    subscriber1DurableSubscriber, subscriber2DurableSubscriber;
+      subscriber1DurableSubscriber, subscriber2DurableSubscriber;
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        publisherPublishSubscribe = new Publisher();
-        publisherPublishSubscribe.create("publisher-publishsubscribe",
-                "publishsubscribe.t");
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    publisherPublishSubscribe = new Publisher();
+    publisherPublishSubscribe.create("publisher-publishsubscribe",
+        "publishsubscribe.t");
 
-        publisherDurableSubscriber = new Publisher();
-        publisherDurableSubscriber.create("publisher-durablesubscriber",
-                "durablesubscriber.t");
+    publisherDurableSubscriber = new Publisher();
+    publisherDurableSubscriber.create("publisher-durablesubscriber",
+        "durablesubscriber.t");
 
-        subscriberPublishSubscribe = new DurableSubscriber();
-        subscriberPublishSubscribe.create("subscriber-publishsubscribe",
-                "publishsubscribe.t", "publishsubscribe");
+    subscriberPublishSubscribe = new DurableSubscriber();
+    subscriberPublishSubscribe.create("subscriber-publishsubscribe",
+        "publishsubscribe.t", "publishsubscribe");
 
-        subscriber1DurableSubscriber = new DurableSubscriber();
-        subscriber1DurableSubscriber.create("subscriber1-durablesubscriber",
-                "durablesubscriber.t", "durablesubscriber1");
+    subscriber1DurableSubscriber = new DurableSubscriber();
+    subscriber1DurableSubscriber.create(
+        "subscriber1-durablesubscriber", "durablesubscriber.t",
+        "durablesubscriber1");
 
-        subscriber2DurableSubscriber = new DurableSubscriber();
-        subscriber2DurableSubscriber.create("subscriber2-durablesubscriber",
-                "durablesubscriber.t", "durablesubscriber2");
+    subscriber2DurableSubscriber = new DurableSubscriber();
+    subscriber2DurableSubscriber.create(
+        "subscriber2-durablesubscriber", "durablesubscriber.t",
+        "durablesubscriber2");
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+    publisherPublishSubscribe.closeConnection();
+    publisherDurableSubscriber.closeConnection();
+
+    // remove the durable subscriptions
+    subscriberPublishSubscribe.removeDurableSubscriber();
+    subscriber1DurableSubscriber.removeDurableSubscriber();
+    subscriber2DurableSubscriber.removeDurableSubscriber();
+
+    subscriberPublishSubscribe.closeConnection();
+    subscriber1DurableSubscriber.closeConnection();
+    subscriber2DurableSubscriber.closeConnection();
+  }
+
+  @Test
+  public void testGetGreeting() {
+    try {
+      publisherPublishSubscribe.sendName("Peregrin", "Took");
+
+      String greeting1 = subscriberPublishSubscribe.getGreeting(1000);
+      assertEquals("Hello Peregrin Took!", greeting1);
+
+      String greeting2 = subscriberPublishSubscribe.getGreeting(1000);
+      assertEquals("no greeting", greeting2);
+
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
     }
+  }
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-        publisherPublishSubscribe.closeConnection();
-        publisherDurableSubscriber.closeConnection();
+  @Test
+  public void testDurableSubscriber() {
+    try {
+      // durable subscriptions, receive messages sent while the
+      // subscribers are not active
+      subscriber2DurableSubscriber.closeConnection();
 
-        // remove the durable subscriptions
-        subscriberPublishSubscribe.removeDurableSubscriber();
-        subscriber1DurableSubscriber.removeDurableSubscriber();
-        subscriber2DurableSubscriber.removeDurableSubscriber();
+      publisherDurableSubscriber.sendName("Bilbo", "Baggins");
 
-        subscriberPublishSubscribe.closeConnection();
-        subscriber1DurableSubscriber.closeConnection();
-        subscriber2DurableSubscriber.closeConnection();
+      // recreate a connection for the durable subscription
+      subscriber2DurableSubscriber.create(
+          "subscriber2-durablesubscriber", "durablesubscriber.t",
+          "durablesubscriber2");
+
+      publisherDurableSubscriber.sendName("Frodo", "Baggins");
+
+      String greeting1 =
+          subscriber1DurableSubscriber.getGreeting(1000);
+      assertEquals("Hello Bilbo Baggins!", greeting1);
+      String greeting2 =
+          subscriber2DurableSubscriber.getGreeting(1000);
+      assertEquals("Hello Bilbo Baggins!", greeting2);
+
+      String greeting3 =
+          subscriber1DurableSubscriber.getGreeting(1000);
+      assertEquals("Hello Frodo Baggins!", greeting3);
+      String greeting4 =
+          subscriber2DurableSubscriber.getGreeting(1000);
+      assertEquals("Hello Frodo Baggins!", greeting4);
+
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
     }
-
-    @Test
-    public void testGetGreeting() {
-        try {
-            publisherPublishSubscribe.sendName("Peregrin", "Took");
-
-            String greeting1 = subscriberPublishSubscribe.getGreeting(1000);
-            assertEquals("Hello Peregrin Took!", greeting1);
-
-            String greeting2 = subscriberPublishSubscribe.getGreeting(1000);
-            assertEquals("no greeting", greeting2);
-
-        } catch (JMSException e) {
-            fail("a JMS Exception occurred");
-        }
-    }
-
-    @Test
-    public void testDurableSubscriber() {
-        try {
-            // durable subscriptions, receive messages sent while the
-            // subscribers are not active
-            subscriber2DurableSubscriber.closeConnection();
-
-            publisherDurableSubscriber.sendName("Bilbo", "Baggins");
-
-            // recreate a connection for the durable subscription
-            subscriber2DurableSubscriber.create(
-                    "subscriber2-durablesubscriber", "durablesubscriber.t",
-                    "durablesubscriber2");
-
-            publisherDurableSubscriber.sendName("Frodo", "Baggins");
-
-            String greeting1 = subscriber1DurableSubscriber.getGreeting(1000);
-            assertEquals("Hello Bilbo Baggins!", greeting1);
-            String greeting2 = subscriber2DurableSubscriber.getGreeting(1000);
-            assertEquals("Hello Bilbo Baggins!", greeting2);
-
-            String greeting3 = subscriber1DurableSubscriber.getGreeting(1000);
-            assertEquals("Hello Frodo Baggins!", greeting3);
-            String greeting4 = subscriber2DurableSubscriber.getGreeting(1000);
-            assertEquals("Hello Frodo Baggins!", greeting4);
-
-        } catch (JMSException e) {
-            fail("a JMS Exception occurred");
-        }
-    }
+  }
 }
